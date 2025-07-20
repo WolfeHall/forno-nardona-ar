457743f59f5283dd2e7a75bf31992afbefdf6dbc
WolfeHall <Marie.Hall.CPCU@gmail.com>
2025-07-20 15:10:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let oven = null;
+const loader = new THREE.GLTFLoader();
+const scene = document.querySelector('a-scene').object3D;
+
+function loadOven(name) {
+  loader.load(`assets/${name}.glb`, function (gltf) {
+    if (oven) {
+      scene.remove(oven);
+      oven.traverse((obj) => {
+        if (obj.material) obj.material.dispose();
+        if (obj.geometry) obj.geometry.dispose();
+      });
+    }
+    oven = gltf.scene;
+    oven.scale.set(1, 1, 1);
+    oven.position.set(0, 0, -1);
+    scene.add(oven);
+  }, undefined, function (error) {
+    console.error("GLB load error:", error);
+  });
+}
+
+function changeOvenColor(colorHex) {
+  if (!oven) return;
+  oven.traverse((node) => {
+    if (node.isMesh && node.material) {
+      node.material.color.set(colorHex);
+      node.material.needsUpdate = true;
+    }
+  });
+}
