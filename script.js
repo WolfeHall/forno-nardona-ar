67836670a7b8f220b5189e130ee0a934e056ea21
WolfeHall <Marie.Hall.CPCU@gmail.com>
2025-07-20 22:19:59 -0400

    Update script.js
    yes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const scene = document.querySelector('a-scene').object3D;
 
 function loadOven(name) {
   console.log("Loading oven:", name);
-  const url = `https://symphonious-fudge-6d9c6b.netlify.app/${name}.glb`;
+const url = `https://euphonious-mooncake-d44cdc.netlify.app/${name}.glb`;
   loader.load(
     url,
     function (gltf) {
